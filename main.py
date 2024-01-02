cfbbb92f6964e589d5b4b02d95e05d70364f1d21
guang384 <guang384@qq.com>
2024-01-02 11:35:44 +0800

    1.Adjusted the filename for token statistics.  2.Also carried out redundant code cleanup

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ dingtalk = DingtalkClient()
 openai = OpenaiClient(if_stream=True)
 
 # use local file to persistent token usage
-token_usage_accumulator = PersistentAccumulator(prefix='token_usage')
+token_usage_accumulator = PersistentAccumulator(prefix='token_usage_'+openai.chat_model)
 
 
 @app.post("/")
@@ -135,112 +135,6 @@ async def root(request: Request):
     return {
         "msgtype": "empty"
     }
-
-
-async def stream_openai(session_webhook, messages, model='gpt-4'):
-    prompt_tokens = num_tokens_from_messages(messages, tiktoken_encoding_tokens_model)
-    print("Estimated Prompt Tokens:", prompt_tokens)
-
-    usage = 0
-    answer = ''
-
-    # call openai
-    start_time = time.perf_counter()
-    try:
-        completion = openai.chat_completions(messages)
-        if_first_answer = True
-        if_in_block = False
-        for chunk in completion:
-            if if_first_answer:
-                chunk_time = time.perf_counter() - start_time
-                print(f"Message received start at {chunk_time:.2f} seconds")
-                if_first_answer = False
-            chunk_message = chunk.choices[0].delta.content
-            if chunk_message is not None and len(chunk_message) > 0 and len(answer.strip()) + len(
-                    chunk_message.strip()) > 0:
-                answer += str(chunk_message)
-                if answer.rstrip().endswith('```') and not if_in_block:
-                    if_in_block = not if_in_block
-                elif answer.rstrip().endswith('```') and if_in_block:
-                    if_in_block = not if_in_block
-                    print("[{}]: {}".format(model, answer.rstrip().replace("\n", "\n  | ")))
-                    try:
-                        await dingtalk.send_text(answer, session_webhook)
-                    except Exception as e:
-                        print("Send answer to dingtalk Failed", e.args)
-                        continue
-                    usage += num_tokens_from_string(answer, tiktoken_encoding_tokens_model)
-                    answer = ''
-                elif answer.endswith('\n\n') and not if_in_block:
-                    print("[{}]: {}".format(model, answer.rstrip().replace("\n", "\n  | ")))
-                    try:
-                        await dingtalk.send_text(answer, session_webhook)
-                    except Exception as e:
-                        print("Send answer to dingtalk Failed", e.args)
-                        continue
-                    usage += num_tokens_from_string(answer, tiktoken_encoding_tokens_model)
-                    answer = ''
-
-        if len(answer) > 0:
-            print("[{}]: {}".format(model, answer.rstrip().replace("\n", "\n  | ")))
-            await dingtalk.send_text(answer + "\n(完)", session_webhook)
-            usage += num_tokens_from_string(answer, tiktoken_encoding_tokens_model)
-
-    except Exception as e:
-        openai_end_time = time.perf_counter()
-        print("Error Request duration: openai {:.3f} s.".format((openai_end_time - openai_start_time)))
-
-        logging.error(e)
-        # traceback.print_exc()
-        await dingtalk.send_markdown(
-            "我错了 orz",
-            "<font color=silver>完，出错啦！暂时没法用咯…… 等会再试试吧 [傻笑] <br />（%s）" % e.args,
-            session_webhook)
-
-    end_time = time.perf_counter()
-
-    token_usage_accumulator.add(prompt_tokens)
-    token_usage_accumulator.add(usage)
-
-    print("Request duration: openai {:.3f} s. Estimated completion Tokens: {}. Token statistics: {}".format(
-        (end_time - start_time), usage, token_usage_accumulator.get_current_total()))
-
-
-async def call_openai(session_webhook, messages, model='gpt-4'):
-    print("Estimated Prompt Tokens:", num_tokens_from_messages(messages, tiktoken_encoding_tokens_model))
-
-    usage = None
-    answer = ''
-    # call openai
-    openai_start_time = time.perf_counter()
-
-    try:
-        completion = openai.chat_completions(messages)
-        openai_end_time = time.perf_counter()
-        print("Request duration: openai {:.3f} s.".format((openai_end_time - openai_start_time)))
-
-        # organize responses
-        answer = completion.choices[0].message.content
-        usage = dict(completion).get('usage')
-        token_usage_accumulator.add(usage.total_tokens)
-
-        await dingtalk.send_text(answer, session_webhook)
-        print("Estimated Completion Tokens:", num_tokens_from_string(answer, tiktoken_encoding_tokens_model))
-
-    except Exception as e:
-        openai_end_time = time.perf_counter()
-        print("Error Request duration: openai {:.3f} s.".format((openai_end_time - openai_start_time)))
-
-        logging.error(e)
-        traceback.print_exc()
-        answer = "<font color=silver>完，出错啦！暂时没法用咯…… 等会再试试吧 [傻笑] <br />（%s）" % e.args
-        await dingtalk.send_markdown("我错了 orz", answer, session_webhook)
-
-    print("[{}]: {}".format(model, answer.replace("\n", "\n  | ")))
-
-    # logging
-    print("{}. Token statistics: {}".format(usage, token_usage_accumulator.get_current_total()))
-
 
 if __name__ == '__main__':
     import uvicorn
